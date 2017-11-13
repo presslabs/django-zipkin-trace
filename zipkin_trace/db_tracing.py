7e3cc64a9c92b532ad74dac3f70e342c5cd1b92b
Calin Don <calin.don@gmail.com>
2017-11-13 20:08:27 +0200

    Add peer to db tracer

--- zipkin_trace/db_tracing.py
+++ zipkin_trace/db_tracing.py
@@ -1,21 +1,39 @@
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.backends.utils import CursorWrapper
-from py_zipkin.zipkin import zipkin_span
+from py_zipkin.zipkin import zipkin_client_span
+
 
 class ZipkinCursorWrapper(CursorWrapper):
 	def execute(self, sql, params=()):
-		with zipkin_span(service_name='SQL', span_name='db-query', binary_annotations=self.get_binary_annotations(sql)):
+		with zipkin_client_span(service_name=self.get_db_type(), span_name=self.get_operation(sql), binary_annotations=self.get_binary_annotations(sql)) as span:
+			self.add_sa_binary_annotation(span)
 			return self.cursor.execute(sql, params)
 
 	def executemany(self, sql, param_list):
-		with zipkin_span(service_name='SQL', span_name='db-query', binary_annotations=self.get_binary_annotations(sql)):
+		with zipkin_client_span(service_name=self.get_db_type(), span_name=self.get_operation(sql), binary_annotations=self.get_binary_annotations(sql)) as span:
+			self.add_sa_binary_annotation(span)
 			return self.cursor.executemany(sql, param_list)
+
+	def get_db_type(self):
+		try:
+			return self.db.vendor
+		except:
+			return 'SQL'
+
+	def get_operation(self, statement):
+		space_idx = statement.find(' ')
+		if space_idx == -1:
+			operation = 'db-query'  # unrecognized format of the query
+		else:
+			operation = statement[0:space_idx]
+		return operation
+
 
 	def get_binary_annotations(self, sql):
 		out = {
 			'db.instance': self.db.alias,
 			'db.statement': sql,
-			'db.type': 'sql',
+			'db.type': self.get_db_type().lower(),
 		}
 
 		try:
@@ -24,6 +42,19 @@ class ZipkinCursorWrapper(CursorWrapper):
 			pass
 
 		return out
+
+	def add_sa_binary_annotation(self, span):
+		default_port = 0
+
+		db_class = self.db.settings_dict.get('ENGINE', '').lower()
+		if 'mysql' in db_class:
+			default_port = 3306
+		elif 'postgres' in db_class:
+			default_port = 5432
+
+		host = self.db.settings_dict.get('HOST', '') or 'localhost'
+		port = int(self.db.settings_dict.get('PORT', default_port) or default_port)
+		span.add_sa_binary_annotation(port, host)
 
 def wrapped_cursor(original):
 	def wrapped(self, *args, **kwargs):
