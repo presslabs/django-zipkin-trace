0dfc1b10e057c653fd1b38a269012df791a43450
Vlad Temian <vladtemian@gmail.com>
2020-06-03 14:12:55 +0300

    Update dependencies

--- zipkin_trace/db_tracing.py
+++ zipkin_trace/db_tracing.py
@@ -4,64 +4,73 @@ from py_zipkin.zipkin import zipkin_client_span
 
 
 class ZipkinCursorWrapper(CursorWrapper):
-	def execute(self, sql, params=()):
-		with zipkin_client_span(service_name=self.get_db_type(), span_name=self.get_operation(sql), binary_annotations=self.get_binary_annotations(sql)) as span:
-			self.add_sa_binary_annotation(span)
-			return self.cursor.execute(sql, params)
+    def execute(self, sql, params=()):
+        with zipkin_client_span(
+            service_name=self.get_db_type(),
+            span_name=self.get_operation(sql),
+            binary_annotations=self.get_binary_annotations(sql),
+        ) as span:
+            self.add_sa_binary_annotation(span)
+            return self.cursor.execute(sql, params)
 
-	def executemany(self, sql, param_list):
-		with zipkin_client_span(service_name=self.get_db_type(), span_name=self.get_operation(sql), binary_annotations=self.get_binary_annotations(sql)) as span:
-			self.add_sa_binary_annotation(span)
-			return self.cursor.executemany(sql, param_list)
+    def executemany(self, sql, param_list):
+        with zipkin_client_span(
+            service_name=self.get_db_type(),
+            span_name=self.get_operation(sql),
+            binary_annotations=self.get_binary_annotations(sql),
+        ) as span:
+            self.add_sa_binary_annotation(span)
+            return self.cursor.executemany(sql, param_list)
 
-	def get_db_type(self):
-		try:
-			return self.db.vendor
-		except:
-			return 'SQL'
+    def get_db_type(self):
+        try:
+            return self.db.vendor
+        except:
+            return "SQL"
 
-	def get_operation(self, statement):
-		space_idx = statement.find(' ')
-		if space_idx == -1:
-			operation = 'db-query'  # unrecognized format of the query
-		else:
-			operation = statement[0:space_idx]
-		return operation
+    def get_operation(self, statement):
+        space_idx = statement.find(" ")
+        if space_idx == -1:
+            operation = "db-query"  # unrecognized format of the query
+        else:
+            operation = statement[0:space_idx]
+        return operation
 
+    def get_binary_annotations(self, sql):
+        out = {
+            "db.instance": self.db.alias,
+            "db.statement": sql,
+            "db.type": self.get_db_type().lower(),
+        }
 
-	def get_binary_annotations(self, sql):
-		out = {
-			'db.instance': self.db.alias,
-			'db.statement': sql,
-			'db.type': self.get_db_type().lower(),
-		}
+        try:
+            out["db.user"] = self.db.settings_dict["USER"]
+        except KeyError:
+            pass
 
-		try:
-			out['db.user'] = self.db.settings_dict['USER']
-		except KeyError:
-			pass
+        return out
 
-		return out
+    def add_sa_binary_annotation(self, span):
+        default_port = 0
 
-	def add_sa_binary_annotation(self, span):
-		default_port = 0
+        db_class = self.db.settings_dict.get("ENGINE", "").lower()
+        if "mysql" in db_class:
+            default_port = 3306
+        elif "postgres" in db_class:
+            default_port = 5432
 
-		db_class = self.db.settings_dict.get('ENGINE', '').lower()
-		if 'mysql' in db_class:
-			default_port = 3306
-		elif 'postgres' in db_class:
-			default_port = 5432
+        host = self.db.settings_dict.get("HOST", "") or "localhost"
+        port = int(self.db.settings_dict.get("PORT", default_port) or default_port)
+        span.add_sa_binary_annotation(port, host)
 
-		host = self.db.settings_dict.get('HOST', '') or 'localhost'
-		port = int(self.db.settings_dict.get('PORT', default_port) or default_port)
-		span.add_sa_binary_annotation(port, host)
 
 def wrapped_cursor(original):
-	def wrapped(self, *args, **kwargs):
-		cursor = original(self, *args, **kwargs)
-		return ZipkinCursorWrapper(cursor, self)
+    def wrapped(self, *args, **kwargs):
+        cursor = original(self, *args, **kwargs)
+        return ZipkinCursorWrapper(cursor, self)
 
-	return wrapped
+    return wrapped
+
 
 def init():
-	BaseDatabaseWrapper.cursor = wrapped_cursor(BaseDatabaseWrapper.cursor)
+    BaseDatabaseWrapper.cursor = wrapped_cursor(BaseDatabaseWrapper.cursor)
--- zipkin_trace/urllib3_tracing.py
+++ zipkin_trace/urllib3_tracing.py
@@ -1,55 +1,58 @@
 try:
-	from urllib.parse import urlparse
+    from urllib.parse import urlparse
 except ImportError:
-	from urlparse import urlparse
+    from urlparse import urlparse
 
 from django.conf import settings
 from urllib3.connectionpool import HTTPConnectionPool, HTTPSConnectionPool
 from py_zipkin.zipkin import zipkin_client_span, create_http_headers_for_new_span
 from py_zipkin.thrift import zipkin_core
 
+
 def wrap_urlopen(func):
-	parsed_host = None
-	if not hasattr(settings, 'ZIPKIN_SERVER'):
-		parsed_host = urlparse(settings.ZIPKIN_SERVER)
+    parsed_host = None
+    if not hasattr(settings, "ZIPKIN_SERVER"):
+        parsed_host = urlparse(settings.ZIPKIN_SERVER)
 
-	def urlopen(self, method, url, **kw):
-		if parsed_host and self.host == parsed_host.hostname:
-			# Don't trace zipkin calls
-			return func(self, method, url, **kw)
+    def urlopen(self, method, url, **kw):
+        if parsed_host and self.host == parsed_host.hostname:
+            # Don't trace zipkin calls
+            return func(self, method, url, **kw)
 
-		attrs = {
-			zipkin_core.HTTP_HOST: self.host,
-			zipkin_core.HTTP_METHOD: method,
-			zipkin_core.HTTP_PATH: url,
-		}
+        attrs = {
+            zipkin_core.HTTP_HOST: self.host,
+            zipkin_core.HTTP_METHOD: method,
+            zipkin_core.HTTP_PATH: url,
+        }
 
-		with zipkin_client_span(service_name=self.host, span_name=self._absolute_url(url), binary_annotations=attrs) as span:
-			headers = kw.pop('headers', {})
-			headers.update(create_http_headers_for_new_span())
+        with zipkin_client_span(
+            service_name=self.host, span_name=self._absolute_url(url), binary_annotations=attrs
+        ) as span:
+            headers = kw.pop("headers", {})
+            headers.update(create_http_headers_for_new_span())
 
-			try:
-				out = func(self, method, url, headers=headers, **kw)
+            try:
+                out = func(self, method, url, headers=headers, **kw)
 
-				if hasattr(out.connection, 'sock') and hasattr(out.connection.sock, 'getpeername'):
-					peer = out.connection.sock.getpeername()
-					span.add_sa_binary_annotation(peer[1], self.host, peer[0])
-				else:
-					span.add_sa_binary_annotation(self.port, self.host)
-			except:
-				# always add sa_binary even in case of error
-				# but if we do it before firing urlopen, then we ended up with two annotations
-				span.add_sa_binary_annotation(self.port, self.host)
-				raise
+                if hasattr(out.connection, "sock") and hasattr(out.connection.sock, "getpeername"):
+                    peer = out.connection.sock.getpeername()
+                    span.add_sa_binary_annotation(peer[1], self.host, peer[0])
+                else:
+                    span.add_sa_binary_annotation(self.port, self.host)
+            except:
+                # always add sa_binary even in case of error
+                # but if we do it before firing urlopen, then we ended up with two annotations
+                span.add_sa_binary_annotation(self.port, self.host)
+                raise
 
-			span.update_binary_annotations({
-				zipkin_core.HTTP_STATUS_CODE: out.status,
-				'http.retries': out.retries.total,
-			})
+            span.update_binary_annotations(
+                {zipkin_core.HTTP_STATUS_CODE: out.status, "http.retries": out.retries.total,}
+            )
 
-		return out
+        return out
 
-	return urlopen
+    return urlopen
+
 
 def init():
-	HTTPConnectionPool.urlopen = wrap_urlopen(HTTPConnectionPool.urlopen)
+    HTTPConnectionPool.urlopen = wrap_urlopen(HTTPConnectionPool.urlopen)
